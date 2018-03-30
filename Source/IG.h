6d7b33690dda859518bd537a03ab849777113f0b
Yusuke Ohashi <github@junkpiano.me>
2018-03-30 12:15:52 +0900

    changes structures again to resolve dependency.
    - added KeychainAccess.

---
+++ Source/IG.h
@@ -0,0 +1,19 @@
+//
+//  IG.h
+//  IG
+//
+//  Created by Yusuke Ohashi on 2018/03/29.
+//  Copyright © 2018 Yusuke Ohashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IG.
+FOUNDATION_EXPORT double IGVersionNumber;
+
+//! Project version string for IG.
+FOUNDATION_EXPORT const unsigned char IGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IG/PublicHeader.h>
+
+
